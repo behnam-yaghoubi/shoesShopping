b1007f76457ac3666280245ac4e0b3c4afe1ca33
behnam-yaghoubi <black.snow.ir@gmail.com>
2020-05-03 15:43:47 +0430

    create menu & footer components

--- src/screens/MasterLayout.js
+++ src/screens/MasterLayout.js
@@ -1,50 +1,25 @@
 import React from 'react';
-import { Button, Typography } from '@material-ui/core';
+import { BrowserRouter } from 'react-router-dom';
+import { Hidden } from '@material-ui/core';
+import useStyles from './MasterLayoutStyles';
+import DesktopMenu from '../components/Menus/desktop';
+import MobileMenu from '../components/Menus/mobile';
+import Footer from '../components/Footer';
 
 export default function MasterLayout() {
+  const classes = useStyles();
+
   return (
-    <div style={{ padding: 25 }}>
-      <Button variant="contained" color="secondary">
-        دوم
-      </Button>
-      <Button variant="contained" color="primary">
-        اول
-      </Button>
-      <Typography color="textPrimary" variant="h1">
-        عنوان
-      </Typography>
-      <Typography color="textPrimary" variant="h2">
-        عنوان
-      </Typography>
-      <Typography color="textPrimary" variant="h3">
-        عنوان
-      </Typography>
-      <Typography color="textPrimary" variant="h4">
-        عنوان
-      </Typography>
-      <Typography color="textPrimary" variant="h5">
-        عنوان
-      </Typography>
-      <Typography color="textPrimary" variant="h6">
-        عنوان
-      </Typography>
-      <Typography color="textPrimary" variant="body1">
-        این پاراگراف اول است
-      </Typography>
-      <Typography color="textPrimary" variant="body2">
-        این پاراگراف اول است
-      </Typography>
-      <Typography color="textPrimary" variant="subtitle1">
-        این زیر عنوان است
-      </Typography>
-      <Typography color="textPrimary" variant="subtitle2">
-        این زیر عنوان است
-      </Typography>
-      <Typography color="textPrimary" variant="caption">
-        زیر پست
-      </Typography>
-      <br />
-      <Typography variant="overline">تخفیف</Typography>
-    </div>
+    <BrowserRouter>
+      <div className={classes.root}>
+        <Hidden xsDown>
+          <DesktopMenu />
+        </Hidden>
+        <Hidden smUp>
+          <MobileMenu />
+        </Hidden>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
