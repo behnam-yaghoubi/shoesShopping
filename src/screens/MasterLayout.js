4e7d4702c890bc4aa8c5053a32fedf21f437269f
behnam-yaghoubi <black.snow.ir@gmail.com>
2020-05-03 15:19:40 +0430

    create theme & config

--- src/screens/MasterLayout.js
+++ src/screens/MasterLayout.js
@@ -1,5 +1,50 @@
 import React from 'react';
+import { Button, Typography } from '@material-ui/core';
 
 export default function MasterLayout() {
-  return <div style={{ textAlign: 'center' }}>خوش آمدید</div>;
+  return (
+    <div style={{ padding: 25 }}>
+      <Button variant="contained" color="secondary">
+        دوم
+      </Button>
+      <Button variant="contained" color="primary">
+        اول
+      </Button>
+      <Typography color="textPrimary" variant="h1">
+        عنوان
+      </Typography>
+      <Typography color="textPrimary" variant="h2">
+        عنوان
+      </Typography>
+      <Typography color="textPrimary" variant="h3">
+        عنوان
+      </Typography>
+      <Typography color="textPrimary" variant="h4">
+        عنوان
+      </Typography>
+      <Typography color="textPrimary" variant="h5">
+        عنوان
+      </Typography>
+      <Typography color="textPrimary" variant="h6">
+        عنوان
+      </Typography>
+      <Typography color="textPrimary" variant="body1">
+        این پاراگراف اول است
+      </Typography>
+      <Typography color="textPrimary" variant="body2">
+        این پاراگراف اول است
+      </Typography>
+      <Typography color="textPrimary" variant="subtitle1">
+        این زیر عنوان است
+      </Typography>
+      <Typography color="textPrimary" variant="subtitle2">
+        این زیر عنوان است
+      </Typography>
+      <Typography color="textPrimary" variant="caption">
+        زیر پست
+      </Typography>
+      <br />
+      <Typography variant="overline">تخفیف</Typography>
+    </div>
+  );
 }
---
+++ src/theme/palette.js
@@ -0,0 +1,15 @@
+export default {
+  primary: {
+    main: '#324958',
+    contrastText: '#fff'
+  },
+  secondary: {
+    main: '#263943',
+    contrastText: '#87e4b0'
+  },
+  text: {
+    primary: '#FFF',
+    secondary: '#177a8d',
+    disabled: '#b18080'
+  }
+};
